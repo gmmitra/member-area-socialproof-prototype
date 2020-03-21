5adbd331fa671f31890919d724a488664b29a034
Jott <jslashx@gmail.com>
2020-03-22 06:32:21 +0800

    socialproof

---
+++ resources/js/components/SocialProofCommunity.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <!-- social proof list -->
+    <div class="container mx-auto p-4">
+        <div class="flex flex-wrap items-stretch proof-wrap">
+           <div class="text-center w-2/3 mx-auto mt-10">
+           <h3 class="text-grey-darkest text-2xl mb-4">NO UPLOAD YET</h3>
+           <p class="mb-4 leading-normal">Join the rest of the members who uploaded their social proof at PanelPlace. It will help others to find out the reliable opportunities out there. </p>
+           </div>
+        </div>
+     
+    </div>
+    <!-- /social proof list -->
+
+  
+
+    </div>
+</template>
+
+<script>
+
+export default {
+   
+    data() {
+            return {
+           
+            }
+        }
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
